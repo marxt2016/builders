b8e05ee6210128678a5756961272e02be54771fe
Olga Melnikova <omelnikova@accesssoftek.com>
2021-11-12 09:50:49 +0400

    initial rollup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./index.css";
+import JS_IMG from '../assets/490_js.jpg';
+
+console.log('Hello');
+
+const image = document.createElement('img');
+image.className = 'js-image';
+image.src = JS_IMG;
+document.body.append(image);
